c7a246df5c1b5ebf54772b4ab9d0299d440238c5
Ivan Smirnov <ivan.smirnov.95.2@gmail.com>
2016-04-10 17:09:42 +0300

    Initial Commit

---
+++ UTSdz2v2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  UTSdz2v2
+//
+//  Created by Smirnov Ivan on 10.04.16.
+//  Copyright © 2016 Ivan Smirnov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
